23f9809b7f2108ba4cef1cdbf077532ca5632ecf
pmaiti <pmaiti@paypal.com>
2020-07-01 10:08:29 +0530

    Invoice PDF generate on the fly

--- node-complete-guide/controller/shop.js
+++ node-complete-guide/controller/shop.js
@@ -1,9 +1,10 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
 
 const Product = require("../models/product");
 const Order = require("../models/order");
-const { fstat } = require('fs');
+const { fstat } = require("fs");
 
 exports.getIndex = (req, res, next) => {
   Product.find()
@@ -56,7 +57,7 @@ exports.getProductDetails = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
+    .populate("cart.items.productId")
     .execPopulate()
     .then((user) => {
       const products = user.cart.items;
@@ -74,8 +75,7 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  Order
-    .find({ 'user.userId': req.user._id })
+  Order.find({ "user.userId": req.user._id })
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
@@ -99,15 +99,40 @@ exports.getCheckout = (req, res, next) => {
 
 exports.getOrdersInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  Order.findById(orderId).then(order => {
-    if(!order) {
-      return next(new Error('No order found'));
+  Order.findById(orderId).then((order) => {
+    if (!order) {
+      return next(new Error("No order found"));
     }
-    if(order.user.userId.toString() !== req.user._id.toString()) {
-      return next(new Error('Unauthorized'));
+    if (order.user.userId.toString() !== req.user._id.toString()) {
+      return next(new Error("Unauthorized"));
     }
     const invoiceName = `invoice-${orderId}.pdf`;
-    const invoicePath = path.join('data', 'invoices', invoiceName);
+    const invoicePath = path.join("data", "invoices", invoiceName);
+    const pdfDoc = new PDFDocument();
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      'inline; filename="' + invoiceName + '"'
+    );
+    pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+
+    pdfDoc.fontSize(26).text("Invoice", {
+      underline: true,
+    });
+    pdfDoc.text("-----------------------");
+    let totalPrice = 0;
+    order.products.forEach((prod) => {
+      totalPrice += prod.quantity * prod.product.price;
+      pdfDoc
+        .fontSize(14)
+        .text(
+          `${prod.product.title} - ${prod.quantity} x Rs. ${prod.product.price}`
+        );
+    });
+    pdfDoc.text("-------------------------");
+    pdfDoc.fontSize(20).text(`Total Price: Rs. ${totalPrice}`);
+    pdfDoc.end();
     // fs.readFile(invoicePath, (err, data) => {
     //   if (err) {
     //     return next(err);
@@ -116,10 +141,8 @@ exports.getOrdersInvoice = (req, res, next) => {
     //   res.setHeader('Content-Disposition', 'inline; filename="'+ invoiceName +'"');
     //   res.send(data);
     // });
-    const file = fs.createReadStream(invoicePath);
-    res.setHeader('Content-type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline; filename="'+ invoiceName +'"');
-    file.pipe(res);
+    // const file = fs.createReadStream(invoicePath);
+    // file.pipe(res);
   });
 };
 
@@ -155,16 +178,19 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrderCreate = (req, res, next) => {
   req.user
-    .populate('cart.items.productId')
+    .populate("cart.items.productId")
     .execPopulate()
     .then((user) => {
-      const products = user.cart.items.map(cart => ({ quantity: cart.quantity, product: { ...cart.productId._doc } }));
+      const products = user.cart.items.map((cart) => ({
+        quantity: cart.quantity,
+        product: { ...cart.productId._doc },
+      }));
       const order = new Order({
         products,
         user: {
           email: req.user.email,
           userId: req.user._id,
-        }
+        },
       });
       return order.save();
     })
