b0064f40bb9e18ec2edf5bb40552ec9b7d2e5b26
pmaiti <pmaiti@paypal.com>
2020-08-16 15:04:47 +0530

    Todo API using Deno

--- deno/deno/app.ts
+++ deno/deno/app.ts
@@ -1,6 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 import todosRoutes from './routes/todos.ts';
+import { connect } from './helpers/db_client.ts';
+
+connect();
 
 const app = new Application();
 
@@ -9,7 +12,14 @@ app.use(async (ctx, next) => {
   await next();
 });
 
+app.use(async (ctx, next) => {
+  ctx.response.headers.set('Access-Control-Allow-Origin', '*');
+  ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+  await next();
+});
+
 app.use(todosRoutes.routes());
 app.use(todosRoutes.allowedMethods());
 
-await app.listen({ port: 3000 });
+await app.listen({ port: 8000 });
