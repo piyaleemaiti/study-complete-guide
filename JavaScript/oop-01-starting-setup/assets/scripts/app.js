2ed0eea67142e739c6a871aa3563c829e9d4c9d6
pmaiti <pmaiti@paypal.com>
2020-09-28 12:48:47 +0530

    All error fixed

--- JavaScript/oop-01-starting-setup/assets/scripts/app.js
+++ JavaScript/oop-01-starting-setup/assets/scripts/app.js
@@ -19,8 +19,11 @@ class ElementAttribute {
 }
 
 class Component {
-  constructor(renderHookId) {
+  constructor(renderHookId, shouldRender = true) {
     this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
+    }
   }
   createRootElement(tag, cssName, attributes) {
     const rootElement = document.createElement(tag);
@@ -46,7 +49,12 @@ class ShoppingCart extends Component {
     return sum;
   }
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log('Order now...');
+      console.log(this.items);
+    };
+    this.render();
   }
   addProduct(product) {
     const updatedItem = [...this.items];
@@ -59,14 +67,17 @@ class ShoppingCart extends Component {
       <h2>Total : INR ${0}</h2>
       <button>Order Now!</button>
     `;
+    const orderButton = cartEl.querySelector('button');
+    orderButton.addEventListener('click', this.orderProducts);
     this.totalOut = cartEl.querySelector('h2');
   }
 }
 
 class ProductItem extends Component {
   constructor(product, renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
     this.product = product;
+    this.render();
   }
   addToCart () {
     App.addProductTocart(this.product);
@@ -90,39 +101,52 @@ class ProductItem extends Component {
 }
 
 class ProductList extends Component {
-  products = [
-    new Product(
-      'Nikon D6',
-      'https://www.nikon.co.in///tmp/Asia/4016499630/3857477713/365508689/3015334490/1054978028/443944295/2580402099/3353927964/2212294583.png',
-      '466',
-      'Nikon D6 DSLR'
-    ), new Product(
-      'Nikon 780',
-      'https://www.nikon.co.in///tmp/Asia/4016499630/3857477713/365508689/3015334490/1054978028/626028340/2580402099/3353927964/1334408982.png',
-      '466',
-      'New Nikon 780 DSLR',
-    ),
-  ];
+  #products = [];
 
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.render();
+    this.#fetchProduct();
+  }
+
+  #fetchProduct() {
+    this.#products = [
+      new Product(
+        'Nikon D6',
+        'https://www.nikon.co.in///tmp/Asia/4016499630/3857477713/365508689/3015334490/1054978028/443944295/2580402099/3353927964/2212294583.png',
+        '466',
+        'Nikon D6 DSLR'
+      ), new Product(
+        'Nikon 780',
+        'https://www.nikon.co.in///tmp/Asia/4016499630/3857477713/365508689/3015334490/1054978028/626028340/2580402099/3353927964/1334408982.png',
+        '466',
+        'New Nikon 780 DSLR',
+      ),
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.#products) {
+      new ProductItem(prod, 'prod-list');
+    }
   }
 
   render() {
     this.createRootElement('ul', 'product-list', [new ElementAttribute('id', 'prod-list')]);
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod, 'prod-list');
-      const prodEl = productItem.render();
+    if (this.#products && this.#products.length > 0) {
+      this.renderProducts();
     }
   }
 }
 
 class Shop {
+  constructor() {
+    this.render();
+  }
   render() {
     this.cart = new ShoppingCart('app');
-    this.cart.render();
-    const productList = new ProductList('app');
-    productList.render();
+    new ProductList('app');
   }
 }
 
@@ -130,7 +154,6 @@ class App {
   
   static init() {
     const shop = new Shop();
-    shop.render();
     this.cart = shop.cart;
   }
 
