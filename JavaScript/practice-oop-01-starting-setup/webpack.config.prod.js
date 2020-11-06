3bdd855f2d59d10af3a349629600911d6ea74e9c
pmaiti <pmaiti@paypal.com>
2020-11-06 20:07:58 +0530

    webpack configuration done

--- JavaScript/practice-oop-01-starting-setup/webpack.config.prod.js
+++ JavaScript/practice-oop-01-starting-setup/webpack.config.prod.js
@@ -2,14 +2,14 @@ const path = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  'mode': 'development',
+  'mode': 'production',
   'entry': './src/app.js',
   'output': {
-    'filename': 'app.js',
+    'filename': '[contenthash].js',
     'path': path.resolve(__dirname, 'assets', 'scripts'),
     'publicPath': 'assets/scripts/'
   },
-  'devtool': 'cheap-module-eval-source-map',
+  'devtool': 'cheap-source-map',
   // 'devServer': {
   //   'contentBase': './'
   // }
